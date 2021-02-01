c74b3b1e748350618303793f2d778ef18c7d7687
류한길 <qer7391@naver.com>
2021-02-01 22:06:53 +0900

    fix: BOJ 4963

--- src/boj/Boj_4963.java
+++ src/boj/Boj_4963.java
@@ -18,21 +18,24 @@ public class Boj_4963 {
 		
 		// 메인 반복문: 주어진 지도마다 반복
 		while(true) {
-			// w와 h 입력받기
+			// w와 h 입력받기 (지도의 너비와 높이)
 			StringTokenizer st = new StringTokenizer(br.readLine());
 			w = Integer.parseInt(st.nextToken());
 			h = Integer.parseInt(st.nextToken());
-			count = 0;
-			// 둘 다 0이면 종료
+			
+			// 너비와 높이가 둘 다 0이면 종료
 			if(w==0 && h==0) break;
 			
-			// 패딩을 포함해 지도 저장할 배열
+			// 섬의 개수를 새기 위한 변수
+			count = 0;
+						
+			// 지도를 저장할 배열. 패딩 추가(+2)
 			int[][] map = new int[h+2][w+2];
 			
 			// 방문 여부를 저장할 boolean 배열
 			boolean[][] visited = new boolean[h][w];
 			
-			// 입력 받기
+			// 지도 입력 받기
 			for (int i = 0; i < h; i++) {
 				StringTokenizer st1 = new StringTokenizer(br.readLine());
 				for (int j = 0; j < w; j++) {
@@ -44,37 +47,37 @@ public class Boj_4963 {
 			// 주어진 지도에서 각 지역에 대해 반복
 			for (int i = 0; i < h; i++) {
 				for (int j = 0; j < w; j++) {
-					// 각 지역에 대해
-					// 1. visited 확인
-					if(visited[i+1][j+1]==true)
+					
+					// 방문한 적 있거나 바다 지역이면 탐색x
+					if(visited[i][j]==true || map[i+1][j+1]==0)
 						continue;
 					
-					// 방문한 적 없는 지역이라면 섬의 개수 +1
+					// 방문한 적 없는 각 '땅' 지역에 대해	
+					dfs(map, visited, i+1, j+1);
 					count++;
-					// 2. 방문한 적 없는 지역이라면 이 지역과 연결된 모든 지역 탐색 -> 한 섬이 된다.
-					bfs(map, visited, i+1, j+1);
 				}
 			}
 			System.out.println(count);
 		}
 	}
 	
-	public static void bfs(int[][] map, boolean[][] visited, int r, int c) {
+	public static void dfs(int[][] map, boolean[][] visited, int r, int c) {
 		// 이미 방문한 지역이라면 탐색x
-		if(visited[r][c]==true)
+		if(visited[r-1][c-1]==true)
 			return;
 		
 		// 방문 마킹
-		visited[r][c] = true;
+		visited[r-1][c-1] = true;
 		
 		// 주변 갈 수 있는 곳 탐색(3x3). 패딩을 해둬서 경계 확인 필요x
 		for (int i = 0; i < 3; i++) {
 			for (int j = 0; j < 3; j++) {
 				if(map[r-1+i][c-1+j]==1) {
-					bfs(map, visited, r-1+i, c-1+j);
+					dfs(map, visited, r-1+i, c-1+j);
 				}
 			}
 		}
+
 	}
 
 }
