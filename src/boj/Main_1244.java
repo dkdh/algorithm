699d66acbfc3bc0535204e6dd2ee963c048cce3b
류한길 <qer7391@naver.com>
2021-02-02 18:06:01 +0900

    add : BOJ 1244

--- src/boj/Main_1244.java
+++ src/boj/Main_1244.java
@@ -30,9 +30,14 @@ public class Main_1244 {
 			// 남자면
 			if(st2.nextToken().equals("1")) {
 				s = Integer.parseInt(st2.nextToken());
-				for (int j = 0; j < n; j++) {
-					if((j+1)%s==0)
-						switches[j] = (switches[j] == 1)? 0 : 1;
+//				for (int j = 0; j < n; j++) {
+//					if((j+1)%s==0)
+//						switches[j] = (switches[j] == 1)? 0 : 1;
+//				}
+				int sx = s;
+				while(sx <= n) {
+					switches[sx-1] = switches[sx-1]^1;
+					sx += s;
 				}
 			}
 			// 여자면
@@ -40,35 +45,43 @@ public class Main_1244 {
 				s = Integer.parseInt(st2.nextToken());
 				
 				// 경계 확인
-				int bound = n-s < s-1 ? n-s: s-1;
+//				int bound = n-s < s-1 ? n-s: s-1;
 				
 				// 스위치 번호->인덱스
 				s--;
 				
 				// 받은 번호의 스위치 반전
-				switches[s] = switches[s]==1 ? 0 : 1;
+//				switches[s] = switches[s]==1 ? 0 : 1;
+				switches[s] = switches[s]^1;
 				
 				// 대칭 확인
-				for (int j = 1; j <= bound; j++) {
-					// 대칭인 스위치는 반전
-					if(switches[s-j] == switches[s+j])
-						switches[s-j] = switches[s+j] = switches[s+j]==1? 0: 1;
-					else
-						break;
-				}
+//				for (int j = 1; j <= bound; j++) {
+//					// 대칭인 스위치는 반전
+//					if(switches[s-j] == switches[s+j])
+//						switches[s-j] = switches[s+j] = switches[s+j]==1? 0: 1;
+//					else
+//						break;
+//				}
+				
+				int left = s;
+				int right = s;
+				while(--left>=0 && ++right<n && switches[left]==switches[right])
+					switches[left] = switches[right] = switches[right]^1;
+				
 			}
 		}
 		
 		// 출력
+		StringBuilder sb = new StringBuilder();
 		for (int i = 0; i < n; i++) {
-			
-			System.out.print(switches[i]);
+			sb.append(switches[i]);
 			
 			if((i+1)%20==0)
-				System.out.println();
+				sb.append("\n");
 			else
-				System.out.print(" ");
+				sb.append(" ");
 		}
+		System.out.println(sb.toString());
 	}
 
 }
